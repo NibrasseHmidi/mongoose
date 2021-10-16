2117cf24721eab9a5f14a151d3f21ed6e6f97fd1
NibrasseHmidi <nibrasse.hmidi@gmail.com>
2021-10-16 19:14:48 +0100

    first commit

---
+++ users.js
@@ -0,0 +1,17 @@
+const users = [
+    {
+      id:2,
+  name: "Aicha Hmidi",
+    age: 24,
+    favoriteFoods: ["riz", "chapati"], },
+    {id:3,
+      name: "Nessrine Hmidi",
+    age: 28,
+    favoriteFoods: ["makarouna", "fast-food"],},
+    {id:4,
+      name: "Tassnime Jaffel",
+    age: 2,
+    favoriteFoods: ["makarouna", "riz"],}
+    
+];
+module.exports = users;
